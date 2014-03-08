6a6adfc7ff940d86eb6fb89f7924a4cecd0c5d1c
loganek <marcin.kolny@gmail.com>
2014-03-08 20:41:22 +0100

    moved Frame class to another file

---
+++ src/csharp/Frame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HighFlyers.Protocol
+{
+    public abstract class Frame
+    {
+        protected abstract int FieldCount { get; }
+        public abstract int CurrentSize { get; }
+
+        protected bool[] PreParseData(byte[] data)
+        {
+            UInt16 fieldFlags = BitConverter.ToUInt16(data, 3);
+            var fields = new bool[FieldCount];
+
+            for (int i = 0; i < FieldCount; i++)
+                fields[i] = (fieldFlags & (1 >> i)) != 0;
+
+            return fields;
+        }
+
+        public abstract void Parse(List<byte> bytes);
+    }
+
+}
--- src/csharp/FrameBuilder.cs
+++ src/csharp/FrameBuilder.cs
@@ -1,48 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using HighFlyers.Protocol.Frames;
 
 namespace HighFlyers.Protocol
 {
-    enum FrameType
+    class FrameBuilder
     {
-        Telemetry = 0
-    }
-
-    public abstract class Frame
-    {
-        protected abstract int FieldCount { get; }
-        public abstract int CurrentSize { get; }
-
-        protected void CheckCrcSum(UInt32 crc)
+        private static void CheckCrcSum(UInt32 crc)
         {
             if (crc != 12) // TODO fuck yeah, hardcoded crc!
                 throw new InvalidDataException("Invalid crc sum!");
         }
 
-        protected bool[] PreParseData(byte[] data)
-        {
-            UInt16 fieldFlags = BitConverter.ToUInt16(data, 3);
-            var fields = new bool[FieldCount];
-
-            for (int i = 0; i < FieldCount; i++)
-                fields[i] = (fieldFlags & (1 >> i)) != 0;
-
-            return fields;
-        }
-
-
-        public abstract void Parse(List<byte> bytes);
-    }
-
-    class FrameBuilder
-    {
         public static Frame BuildFrame(List<byte> bytes)
         {
             Frame frame;
             UInt16 size = BitConverter.ToUInt16(bytes.ToArray(), 1);
+
+            CheckCrcSum(BitConverter.ToUInt32(bytes.ToArray(), bytes.Count - 5));
 
             if (size != bytes.Count)
                 throw new InvalidDataException("Invalid data length");
