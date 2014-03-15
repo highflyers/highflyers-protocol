a9b4b35fc81bf89120e2f884667b753cb8fa51e5
loganek <marcin.kolny@gmail.com>
2014-03-15 01:05:37 +0100

    moved non-generated implementation from framebuilder to a header file

--- src/cpp/highflyers-protocol/include/highflyers-protocol/FrameBuilder.h
+++ src/cpp/highflyers-protocol/include/highflyers-protocol/FrameBuilder.h
@@ -20,8 +20,23 @@ namespace Protocol {
 class FrameBuilder
 {
 private:
-	static void check_bytes(const std::vector<byte>& bytes);
-	static void check_crc_sum(uint32 crc);
+	static void check_bytes(const std::vector<byte>& bytes)
+	{
+		BitConverter converter(Endianes::BIG_ENDIANA);
+		uint32 size = converter.from_bytes<uint32>(bytes, 1);
+
+		check_crc_sum(converter.from_bytes<uint32>(bytes, bytes.size() - 5));
+
+		if (size != bytes.size())
+			throw std::length_error("Invalid data length");
+	}
+
+	static void check_crc_sum(uint32 crc)
+	{
+		if (crc != 12) // TODO fuck yeah, hardcoded crc!
+			throw std::logic_error("Invalid crc sum!");
+	}
+	
 	static std::shared_ptr<Frame> build_frame(const std::vector<byte>& bytes);
 };
 
