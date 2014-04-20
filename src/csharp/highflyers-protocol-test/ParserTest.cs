d2814dc8db144db05bb8fb90f359e8fd7b0d7057
Marcin Kolny <marcin.kolny@gmail.com>
2014-04-20 13:26:46 +0200

    added one more test

--- src/csharp/highflyers-protocol-test/ParserTest.cs
+++ src/csharp/highflyers-protocol-test/ParserTest.cs
@@ -8,14 +8,14 @@ namespace HighFlyers.Test.Protocol
 	{
 		[Test]
 		[ExpectedException(typeof(System.IO.InvalidDataException))]
-		public void ShouldThrowExceptionBecauseOfBadData()
+		public void ShouldThrowExceptionBecauseOfBadData ()
 		{
-			var parser = new Parser<FrameBuilder>();
-			parser.AppendBytes(new byte[] { FrameParserHelper.Sentinel, 99 });
+			var parser = new Parser<FrameBuilder> ();
+			parser.AppendBytes (new byte[] { FrameParserHelper.Sentinel, 99 });
 		}
 
 		[Test]
-		public void SimpleParsingData()
+		public void SimpleParsingData ()
 		{
 			var parser = new Parser<FrameBuilder> ();
 
@@ -32,6 +32,26 @@ namespace HighFlyers.Test.Protocol
 			var frame = parser.LastFrame as HighFlyers.Protocol.Frames.TestStruct;
 			Assert.AreEqual (256 + FrameParserHelper.EndFrame, frame.Field1);
 			Assert.AreEqual (2, frame.Field3);
+		}
+
+		[Test]
+		public void CheckNullElement ()
+		{
+			var parser = new Parser<FrameBuilder> ();
+
+			parser.AppendBytes (new byte[] { 
+				0, 1 + 4, 0,
+				5, 1, 0, 0,
+				80, 
+				FrameParserHelper.Sentinel, 12, 0, 0, 0,
+				FrameParserHelper.EndFrame
+			});
+
+			var frame = parser.LastFrame as HighFlyers.Protocol.Frames.TestStruct;
+			Assert.AreEqual (256 + 5, frame.Field1);
+			Assert.IsNull (frame.Field2);
+			Assert.AreEqual (80, frame.Field3);
+			Assert.IsNull (frame.Field4);
 		}
 	}
 }
