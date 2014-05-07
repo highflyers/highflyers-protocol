f0c4bfca74d69e99f909730cac40337fff3084bd
Marcin Kolny <marcin.kolny@gmail.com>
2014-05-07 12:01:57 +0200

    removed SecondStruct

--- src/highflyers-protocol-c/frames.h
+++ src/highflyers-protocol-c/frames.h
@@ -22,15 +22,4 @@ typedef struct
 TestStruct* TestStruct_parse (const byte* data, int size);
 int TestStruct_current_size (const TestStruct* value);
 
-typedef struct
-{
-	TestStruct Field1;
-	byte Field2;
-	TestStruct Field3;
-	bool Field3_enabled;
-} SecondStruct; 
-
-SecondStruct SecondStruct_parse (const byte* bytes, int size);
-int Second_current_size (const SecondStruct* value);
-
 #endif
